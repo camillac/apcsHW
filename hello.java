bdebe5bc583a86b27e74276bbf145a9b053b5eba
Camilla <ccheng6@stuy.edu>
2017-12-04 00:44:07 -0500

    hello.java

---
+++ hello.java
@@ -0,0 +1,16 @@
+/** 
+ *  
+ *  
+ */
+
+public class Template {
+
+    /** 
+     *  
+     *  
+     */
+    public static void main(String[] commandLine) {
+        System.out.println( System.out.println("Hi, Mr. Holmes!");
+
+    }
+}
